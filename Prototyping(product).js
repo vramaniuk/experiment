7f3ac145f1c43448225c0fa13d0cc38e00449933
vramaniuk <astyanax@mail.ru>
2017-06-19 20:27:51 +0300

    fact next

--- Prototyping(product).js
+++ Prototyping(product).js
@@ -1,7 +1,7 @@
 /**
  * Created by VSKD on 28.03.2017.
  */
-/*function Product(name, price, color) {
+function Product(name, price, color) {
     this.name = name;
     this.price = price;
     this.color = color;
@@ -10,47 +10,52 @@
         throw RangeError('Нельзя создать продукт ' +
             this.name + ' с отрицательной ценой');
     }
-
-    return this;
+    this.sayName=()=>
+    {
+        console.log(this.name)
+    }
 }
-
+// Product.prototype.sayName = function () {
+//     console.log(this.name)
+// }
 function Food(name, price, color) {
     Product.call(this, name, price, color);
     this.category = 'еда';
 }
 
-// Food.prototype = Object.create(Product.prototype);
+Food.prototype = Object.create(Product.prototype);
 
 function Toy(name, price, color) {
     Product.call(this, name, price, color);
     this.category = 'игрушка';
 }
 
-Toy.prototype = Object.create(Product.prototype);
+// Toy.prototype = Object.create(Product.prototype);
 
 var cheese = new Food('фета', 5, 'yellow');
 var fun = new Toy('робот', 40, 'red');
 
-console.log(cheese.name);*/
+console.log(cheese.sayName());
+// console.log(fun.name);
 
-class Product {
-    constructor(name,price,color) {
-        this.name = name;
-        this.price = price;
-        this.color = color;
-    }
-}
-class Food extends Product{
-    constructor(name,price,color,category){
-        super(name,price,color);
-        this.category = category;
-        if (price < 0) {
-            throw RangeError('Нельзя создать продукт ' +
-                this.name + ' с отрицательной ценой');
-        }
-    }
-}
-const cheese = new Food('фета', 5, 'yellow','kovno');
-console.log(cheese.category);
-const thms = new Product('fallo', -15, 'green','shedevr');
-console.log(thms.name);
+// class Product {
+//     constructor(name,price,color) {
+//         this.name = name;
+//         this.price = price;
+//         this.color = color;
+//     }
+// }
+// class Food extends Product{
+//     constructor(name,price,color,category){
+//         super(name,price,color);
+//         this.category = category;
+//         if (price < 0) {
+//             throw RangeError('Нельзя создать продукт ' +
+//                 this.name + ' с отрицательной ценой');
+//         }
+//     }
+// }
+// const cheese = new Food('фета', 5, 'yellow','kovno');
+// console.log(cheese.category);
+// const thms = new Product('fallo', -15, 'green','shedevr');
+// console.log(thms.name);
